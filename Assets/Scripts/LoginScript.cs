c5d6da4d709a0e9709f022bb81ef6e2fe4b5111d
Oscar Sánchez Fernández <oscarsanchezfer@gmail.com>
2019-07-13 19:26:44 +0200

    interface

---
+++ Assets/Scripts/LoginScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void changeScene()
+    {
+        GameObject nameInput = GameObject.FindGameObjectWithTag("nameInput");
+       
+        print("HOLA" + nameInput.GetComponent<InputField>().text);
+    }
+}
